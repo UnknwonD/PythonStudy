84885ab1ad96b217c1200e0cc8d791e1799d5811
CoolD <62641441+UnknwonD@users.noreply.github.com>
2022-04-28 06:50:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template, request, redirect, url_for, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
+
+app = Flask("SuperScrapper")
+db = {}
+
+@app.route("/")
+def home():
+  return render_template("potato.html")
+
+@app.route("/report")
+def report():
+  #how to extract query argument
+  word = request.args.get('word')
+
+  if word:
+    word = word.lower()
+    existingJobs = db.get(word)
+    if existingJobs :
+      jobs = existingJobs
+    else:
+      jobs = get_jobs(word)
+      db[word] = jobs
+  else:
+    return redirect(url_for('home'))
+
+  #how to send through datas
+  return render_template("report.html", 
+  searchingBy = word,
+  resultsNumber = len(jobs),
+  jobs = jobs
+  )
+
+@app.route("/export")
+def export():
+  try:
+    word = request.args.get('word')
+    if not word:
+      raise Exception()
+    word = word.lower()
+    jobs = db.get(word)
+    if not jobs:
+      raise Exception()
+    save_to_file(jobs)
+    return send_file("jobs.csv")
+  except:
+    return redirect(url_for('home'))
+    
+
+    
+
+app.run(host="0.0.0.0")
