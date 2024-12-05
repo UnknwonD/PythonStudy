f23ef90366b000c5db16343b849f978275081c2d
CoolD <daeho5000@naver.com>
2024-12-05 23:36:18 +0900

    1260 DFS BFS 문제풀이 완료

--- CodingTest/BOJ/1260.py
+++ CodingTest/BOJ/1260.py
@@ -1,48 +1,49 @@
 from collections import deque
 
-n, m, v = map(int, input().split())
+def dfs(v, adj, visited):
+    stack = [v]
+    dfs_result = []
+    
+    while stack:
+        node = stack.pop()
+        if not visited[node]:
+            visited[node] = True
+            dfs_result.append(node)
+            # 인접 노드를 역순으로 스택에 추가하여 작은 숫자부터 방문
+            stack.extend(sorted(adj[node], reverse=True))
+    
+    return dfs_result
 
-adj = {i:[] for i in range(1, n+1)}
+def bfs(v, adj, visited):
+    queue = deque([v])
+    bfs_result = []
+    visited[v] = True
+    
+    while queue:
+        node = queue.popleft()
+        bfs_result.append(node)
+        # 인접 노드를 정렬하여 큐에 추가하여 작은 숫자부터 방문
+        for next_node in sorted(adj[node]):
+            if not visited[next_node]:
+                visited[next_node] = True
+                queue.append(next_node)
+                
+    return bfs_result
+
+n, m, v = map(int, input().split())
+adj = {i: [] for i in range(1, n + 1)}
 
 for _ in range(m):
     s, e = map(int, input().split())
     adj[s].append(e)
     adj[e].append(s)
 
-print(adj)
-for val in adj.values():
-    sorted(val)
-print(adj)
+# DFS 수행
+visited = [False] * (n + 1)
+dfs_result = dfs(v, adj, visited)
+print(' '.join(map(str, dfs_result)))
 
-stack = []
-visited = [0 for _ in range(n + 1)]
-queue = deque()
-
-# DFS
-dfs_visited = []
-stack.append(v)
-while stack:
-    now = stack.pop()
-    dfs_visited.append(now)
-    visited[now] = 1
-    
-    for next_node in adj[now]:
-        if next_node not in dfs_visited and visited[now] != 1:
-            print(next_node, dfs_visited)
-            stack.append(next_node)
-            visited[next_node] = 1
-
-print(' '.join([str(node) for node in dfs_visited]))
-
-# BFS
-bfs_visited = []
-queue.append(v)
-while queue:
-    cur = queue.popleft()
-    bfs_visited.append(cur)
-    
-    for next_node in adj[cur]:
-        if next_node not in bfs_visited and next_node not in queue:
-            queue.append(next_node)
-
-print(' '.join([str(node) for node in bfs_visited]))
+# BFS 수행
+visited = [False] * (n + 1)
+bfs_result = bfs(v, adj, visited)
+print(' '.join(map(str, bfs_result)))
