a6e5fb471debf51d312f644e1cb8ce18db55812d
UnknwonD <daeho5000@naver.com>
2025-02-13 23:22:51 +0900

    union by rank 1717

--- CodingTest/BOJ/1717.py
+++ CodingTest/BOJ/1717.py
@@ -6,24 +6,26 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a, b):
+def union_parent(parent, rank, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
-    
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-    
+
+    if a != b:
+        if rank[a] > rank[b]:  # 더 깊은 트리에 붙이기
+            parent[b] = a
+        elif rank[a] < rank[b]:
+            parent[a] = b
+        else:
+            parent[b] = a
+            rank[a] += 1  # 같은 높이면 한쪽을 증가
 
 n, m = map(int, input().split())
-
 parent = [i for i in range(n+1)]
+rank = [0] * (n+1)  # 트리 높이를 저장하는 배열
 
-for i in range(m):
+for _ in range(m):
     x, a, b = map(int, input().split())
-
-    if x == 1: # 확인 연산
-        print("NO") if find_parent(parent, a) != find_parent(parent, b) else print("YES")
-    else: # 합집합 연산
-        union_parent(parent, a, b)
+    if x == 1:  # 같은 집합인지 확인
+        print("YES" if find_parent(parent, a) == find_parent(parent, b) else "NO")
+    else:  # 합집합 연산
+        union_parent(parent, rank, a, b)
